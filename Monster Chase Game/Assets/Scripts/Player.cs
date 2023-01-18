8a35964a69d54733eeaf2c66ee6cd0fede74fbe3
tarasinghrajput <TARASINGHRAJPUT7261@GMAIL.COM>
2023-01-18 20:09:07 +0530

    added both players movement and walk animation

--- Monster Chase Game/Assets/Scripts/Player.cs
+++ Monster Chase Game/Assets/Scripts/Player.cs
@@ -4,6 +4,26 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] //it make the private variable to acces from unity editor
+    private float moveForce = 10f;
+
+    [SerializeField] //it make the private variable to acces from unity editor
+    private float jumpForce = 11f;
+
+    private float movementX;
+
+    private Rigidbody2D myBody;
+    private SpriteRenderer sr;
+    private Animator anim;
+    private string WALK_ANIMATION = "Walk";
+
+    private void Awake() 
+    {
+        myBody = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +33,32 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        PlayerMoveKeyboard();
+        AnimatePlayer();
+    }
+
+    void PlayerMoveKeyboard()
+    {
+        movementX = Input.GetAxisRaw("Horizontal");
+
+        transform.position += new Vector3(movementX, 0f, 0f) * Time.deltaTime * moveForce;
+    }
+
+    void AnimatePlayer()
+    {
         
+        // we are going to the right side
+        if(movementX > 0)
+        {
+            anim.SetBool(WALK_ANIMATION, true);
+            sr.flipX = false;
+        } else if(movementX < 0) // we are going to the left side
+        {
+            anim.SetBool(WALK_ANIMATION, true);
+            sr.flipX = true;
+        } else
+        {
+            anim.SetBool(WALK_ANIMATION, false);
+        }
     }
 }
