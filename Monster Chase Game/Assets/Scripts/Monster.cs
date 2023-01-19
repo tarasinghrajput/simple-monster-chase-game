d121b85e2baf54ddb934e0ecb046107a35fd6ecd
tarasinghrajput <tarasinghrajput7261@gmail.com>
2023-01-19 16:00:33 +0530

    frezed the rotation of players at z and added moster script

--- Monster Chase Game/Assets/Scripts/Monster.cs
+++ Monster Chase Game/Assets/Scripts/Monster.cs
@@ -4,6 +4,21 @@ using UnityEngine;
 
 public class Monster : MonoBehaviour
 {
+    [HideInInspector]
+    public float speed;
+    private Rigidbody2D myBody;
+
+    void Awake()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+        speed = 7;
+    }
+
+    void FixedUpdate()
+    {
+        myBody.velocity = new Vector2(speed, myBody.velocity.y);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
